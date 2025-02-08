275f107bafbb856c64b36ac646c9428b942ed687
shazzad <sumonshazzad980.com>
2025-02-09 00:10:30 +0600

    change iii

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,36 @@
+// import mongoose, { Mongoose } from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("MONGODB_URI is not defined");
+// }
+// let cached = global.mongoose;
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+// export const connectToDb = async () => {
+//   if (cached.conn) {
+//     console.log("Connected From Cache");
+//     return cached.conn;
+//   }
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(MONGODB_URI, { dbName: "serviceDB" })
+//       .then((result) => {
+//         console.log("Connected to MongoDB");
+//         return result;
+//       })
+//       .catch((error) => {
+//         console.error("Error connecting to MongoDB", error);
+//         throw error;
+//       });
+//   }
+//   cached.conn = await cached.promise;
+
+//   return cached.conn;
+// };
+
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -5,15 +38,19 @@ const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error("MONGODB_URI is not defined");
 }
+
 let cached = global.mongoose;
+
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
+
 export const connectToDb = async () => {
   if (cached.conn) {
     console.log("Connected From Cache");
     return cached.conn;
   }
+
   if (!cached.promise) {
     cached.promise = mongoose
       .connect(MONGODB_URI, { dbName: "serviceDB" })
@@ -26,6 +63,7 @@ export const connectToDb = async () => {
         throw error;
       });
   }
+
   cached.conn = await cached.promise;
 
   return cached.conn;
