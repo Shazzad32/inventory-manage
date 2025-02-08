73072b690d613fa5eca2f354e30ef0271f1fd345
shazzad <sumonshazzad980.com>
2025-02-09 00:22:50 +0600

    change iiv

--- utils/database.js
+++ utils/database.js
@@ -1,36 +1,3 @@
-// import mongoose, { Mongoose } from "mongoose";
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   throw new Error("MONGODB_URI is not defined");
-// }
-// let cached = global.mongoose;
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-// export const connectToDb = async () => {
-//   if (cached.conn) {
-//     console.log("Connected From Cache");
-//     return cached.conn;
-//   }
-//   if (!cached.promise) {
-//     cached.promise = mongoose
-//       .connect(MONGODB_URI, { dbName: "serviceDB" })
-//       .then((result) => {
-//         console.log("Connected to MongoDB");
-//         return result;
-//       })
-//       .catch((error) => {
-//         console.error("Error connecting to MongoDB", error);
-//         throw error;
-//       });
-//   }
-//   cached.conn = await cached.promise;
-
-//   return cached.conn;
-// };
-
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -94,46 +61,6 @@ export const connectToDb = async () => {
 //   if (!cached.promise) {
 //     cached.promise = mongoose
 //       .connect(MONGODB_URI, { dbName: "devflow" })
-//       .then((result) => {
-//         console.log("Connected to MongoDB");
-//         return result;
-//       })
-//       .catch((error) => {
-//         console.error("Error connecting to MongoDB", error);
-//         throw error;
-//       });
-//   }
-
-//   cached.conn = await cached.promise;
-
-//   return cached.conn;
-// };
-
-// export default connectToDb;
-
-// import mongoose, { Mongoose } from "mongoose";
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   throw new Error("MONGODB_URI is not defined");
-// }
-
-// let cached = global.mongoose;
-
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// export const connectToDb = async () => {
-//   if (cached.conn) {
-//     console.log("Connected From Cache");
-//     return cached.conn;
-//   }
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose
-//       .connect(MONGODB_URI, { dbName: "inventoryDB" })
 //       .then((result) => {
 //         console.log("Connected to MongoDB");
 //         return result;
