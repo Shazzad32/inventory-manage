26991e467180c8946a4ad2ea3569e38ef233134a
shazzad <sumonshazzad980.com>
2026-04-27 15:09:54 +0600

    edit login page

--- app/retail/not-paid-list/page.js
+++ app/retail/not-paid-list/page.js
@@ -2,16 +2,8 @@ import NotPaid from "@/components/NotPaid";
 import axios from "axios";
 import React from "react";
 export const dynamic = "force-dynamic";
-const [search, setSearch] = useState("");
 
 const page = async () => {
-  // const devicesReq = (await axios.get(`${process.env.URL}/api/devices/retail`))
-  //   .data;
-
-  // const devices = devicesReq.filter(
-  //   (item) => item.send_to === "Retail" && item.is_complete === false
-  // );
-
   const res = await fetch(`${process.env.URL}/api/devices/retail`, {
     next: { revalidate: 60 },
   });
@@ -22,21 +14,38 @@ const page = async () => {
     (item) => item.send_to === "Retail" && item.is_complete === false
   );
 
-  const kanaphuliassignIds = (
-    await axios.get(process.env.RETAIL_ASSIGN_ID, {
-      headers: {
-        Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
-      },
-    })
-  ).data;
+  // const kanaphuliassignIds = (
+  //   await axios.get(process.env.RETAIL_ASSIGN_ID, {
+  //     headers: {
+  //       Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
+  //     },
+  //   })
+  // ).data;
 
-  const tiktikiAssingIDs = (
-    await axios.get(process.env.TIKTIKI_ASSING_ID, {
-      headers: {
-        Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
-      },
-    })
-  ).data;
+  // const tiktikiAssingIDs = (
+  //   await axios.get(process.env.TIKTIKI_ASSING_ID, {
+  //     headers: {
+  //       Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
+  //     },
+  //   })
+  // ).data;
+
+  const kanaphuliRes = await fetch(process.env.RETAIL_ASSIGN_ID, {
+    headers: {
+      Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
+    },
+    cache: "no-store",
+  });
+
+  const tiktikiRes = await fetch(process.env.TIKTIKI_ASSING_ID, {
+    headers: {
+      Authorization: "BEARER ####cp-!!!!$$sultantracker.com###",
+    },
+    cache: "no-store",
+  });
+
+  const kanaphuliassignIds = await kanaphuliRes.json();
+  const tiktikiAssingIDs = await tiktikiRes.json();
 
   const totalassignIDs = [
     ...new Set([...kanaphuliassignIds, ...tiktikiAssingIDs]),
